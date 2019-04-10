4db1624b8d15ec9130b6a8120d1336ebca1ac837
Ellanst <49144635+Ellanst@users.noreply.github.com>
2019-04-10 15:48:25 +0800

    Update linear-algebra.py

--- linear-algebra.py
+++ linear-algebra.py
@@ -12,10 +12,33 @@ if row == col and Rank == row:
     print("A only have one solution")
     # 设被求助阵为二维表A[0:n][0:n]
     for i in range(row - 1):
-        # 用A[i][i]将A[i+1:n][i]的值都变为0
 
-    det = 0.0
+        # 将A[i][i] = 0 的调整到下一行，方便消元
+        if A[i][i] == 0:
+            temp = A[i,:].copy() #防止数组改变切片改变，开辟一块新的内存空间，把被拷贝对象中的值复制过去
+            x = 1
+            while A[i + x][i] == 0:
+                x += 1
+            A[i][:] = A[i + x][:]
+            A[i + x][:] = temp
+        else:
+            True
+
+        # 用A[i][i]将A[i+1:n][i]的值都变为0
+        for j in range(i+1,row):
+            n =  A[j][i] / A[i][i]
+            B[j] = B[j] - n * B[i]
+            A[j] = A[j] - n * A[i]
+
+    # 回代
     for i in range(row):
-        det += A[i][i]
+        j = row - 1
+        while j > row-1-i:
+            B[row - 1 - i] = B[row - 1 - i] - det[j]*A[row- 1 - i][j]
+            j -= 1
+        det[row - 1 - i] = B[row - 1 - i] / A[row- 1 - i][row- 1 - i]
+
+    print(det)
+    
 else:
     print("A have no solution or too many solutions")
